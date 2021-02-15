d8029bedda1b6aa394187e2edd6687010829fd24
IuliiaPopova <popovasqa@gmail.com>
2021-02-15 13:10:24 -0800

    Kaban test1

--- kata.py
+++ kata.py
@@ -1,18 +1,12 @@
-# from re import search
-#
-# regex = (
-#     '^'            # start line
-#     '(?=.*\d)'     # must contain one digit from 0-9
-#     '(?=.*[a-z])'  # must contain one lowercase characters
-#     '(?=.*[A-Z])'  # must contain one uppercase characters
-#     '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
-#     '{6,}'         # length at least 6 chars
-#     '$'            # end line
-# )
-# # regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{6,}$"
-#
-# print(bool(search(regex, 'fd3R9')))
+"""
+
+
+
+
+"""
 
 import re
+
+
 print(re.findall(r'[3]?[7]', '1A234 1234447'))
 
